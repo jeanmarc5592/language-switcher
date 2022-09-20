d2f8c2f2e41f9eb1a90d1416dde1c6be332c7d64
Jean-Marc Möckel <jmoeckel@jastech-solutions.de>
2022-09-20 19:29:16 +0200

    Adjust Language Grid

--- src/components/LanguageList/index.tsx
+++ src/components/LanguageList/index.tsx
@@ -1,9 +1,10 @@
 import localesList from '../../data/locales.json';
 import { LanguageButton } from '..';
+import styles from './LanguageList.module.css';
 
 export const LanguageList = () => {
   return (
-    <div>
+    <div className={styles.languageGrid}>
       {localesList.locales.map(locale => {
         return (
           <LanguageButton {...locale} key={locale.code} />
