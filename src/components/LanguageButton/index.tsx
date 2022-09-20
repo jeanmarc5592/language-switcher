a4987dba7c515c88d74c0ad8b8ad5dc51d94b8c3
Jean-Marc Möckel <jmoeckel@jastech-solutions.de>
2022-09-20 19:43:08 +0200

    Render dynamic language flag

--- src/components/LanguageButton/index.tsx
+++ src/components/LanguageButton/index.tsx
@@ -1,4 +1,12 @@
 import styles from './LanguageButton.module.css';
+import bgFlag from '../../icons/bg.svg';
+import deFlag from '../../icons/de.svg';
+import enFlag from '../../icons/en.svg';
+import huFlag from '../../icons/hu.svg';
+import plFlag from '../../icons/pl.svg';
+import roFlag from '../../icons/ro.svg';
+import ruFlag from '../../icons/ru.svg';
+
 type LanguageButtonProps = {
   code: string;
   name: string;
@@ -7,9 +15,30 @@ type LanguageButtonProps = {
 }
 
 export const LanguageButton: React.FC<LanguageButtonProps> = ({ code, name, iso }) => {
+  const getCountryFlag = (countryCode: string): string => {
+    switch (countryCode) {
+      case 'bg':
+        return bgFlag;
+      case 'de':
+        return deFlag;
+      case 'en':
+        return enFlag;
+      case 'hu':
+        return huFlag;
+      case 'pl':
+        return plFlag;
+      case 'ro':
+        return roFlag;
+      case 'ru':
+        return ruFlag;
+      default: 
+        return '';
+    }
+  }
+
   return (
-    <a href="/jo" className={styles.container}>
-      {/* TODO: Add dynamic flag */}
+    <a href={`/${iso.toLowerCase()}`} className={styles.container}>
+      <img width="32px" height="32px" alt={`${code}-flag`} src={getCountryFlag(code)} />
       <p className={styles.countryLabel}>{name}</p>
     </a>
   )
