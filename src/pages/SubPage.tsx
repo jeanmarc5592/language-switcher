1ba2250c05d3e2a5c589c5306a7b41179ab9c627
Jean-Marc Möckel <jmoeckel@jastech-solutions.de>
2022-09-21 15:39:52 +0200

    Render Language based on locale in pathname

--- src/pages/SubPage.tsx
+++ src/pages/SubPage.tsx
@@ -1,8 +1,33 @@
+import { useLocation } from "react-router"
+import localesList from '../data/locales.json';
+
+type LocaleData = {
+  code: string;
+  name: string;
+  iso: string;
+  file: string;
+};
+
+type LocalesListData = {
+  locales: LocaleData[]
+}
 
 export const SubPage = () => {
+  const location = useLocation();
+
+  const getCurrentLanguage = (isoCode: string, localesList: LocalesListData): string => {
+    // Strips the '/' from the isocode
+    const cleanIsoCode = isoCode.replace('/', '');
+    const currentLocale = localesList.locales.find(locale => locale.iso.toLowerCase() === cleanIsoCode);
+    if (!currentLocale) {
+      return `Sorry, but the language ${isoCode} is not available`;
+    }
+    return currentLocale.name;
+  };
+
   return (
     <>
-      <h1>Hello from the SubPage</h1>
+      <h1>{getCurrentLanguage(location.pathname, localesList)}</h1>
     </>
   )
 }
